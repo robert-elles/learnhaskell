86ffc72f6fedfc8cd99cf96d3320863e59ada6fb
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2024-04-08 06:41:51 +0200

    basic dev shell

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # learnhaskell
-Learn Haskell Project
+My repo to keep everything to follow the Haskell Tutorial and begin haskel development
+
+
+### Setup
+
+1. git config user.email 4633229+robert-elles@users.noreply.github.com
+
+direnv allow / reload
+
+
+## Start
+
+[Tutorial and Exercises](https://www.cis.upenn.edu/~cis1940/spring13/lectures.html)
+[Collection of Haskell Tutorials](https://wiki.haskell.org/Tutorials)
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Application packaged using poetry2nix";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ hello ];
+          packages = [
+          ];
+        };
+      }
+    ));
+}
