e2355abaf87ea10b6bc28c8bf1b460c1158f0dbe
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2024-04-10 15:51:20 +0200

    wip ex1 toDigits working

--- exercise1/ex1.hs
+++ exercise1/ex1.hs
@@ -1,9 +1,11 @@
 toDigits :: Integer -> [Integer]
-toDigits n
-    | n <= 0 = []
-    | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
--- toDigitsRev :: Integer -> [Integer]
+toDigits 0 = []
+toDigits n = (toDigits (n `div` 10)) ++ [(n `mod` 10)] 
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = toDigits n
 
 main = do
+    print (toDigits 0)
     print (toDigits 1234)
-    print (toDigits 1234)
+    print (toDigitsRev 1234 == [4,3,2,1])
