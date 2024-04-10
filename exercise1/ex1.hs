4c1c6cf8f4e64831307227646630386b54088e05
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2024-04-10 16:12:48 +0200

    inotifywait to re-execute compile and run

--- exercise1/ex1.hs
+++ exercise1/ex1.hs
@@ -1,11 +1,18 @@
+-- while inotifywait -e modify ex1.h1; do ghc ./ex1.hs && ./ex1; done
+
+import Control.Exception (assert)
+
 toDigits :: Integer -> [Integer]
 toDigits 0 = []
 toDigits n = (toDigits (n `div` 10)) ++ [(n `mod` 10)] 
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev n = toDigits n
+toDigitsRev n = [(n `mod` 10)] ++ (toDigitsRev (n `div` 10))
+
+-- check :: 
+
 
 main = do
     print (toDigits 0)
     print (toDigits 1234)
-    print (toDigitsRev 1234 == [4,3,2,1])
+    -- assert ((toDigitsRev 1234) == [4,3,2,1,0]) "Failed"
